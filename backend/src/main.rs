7c913c77fa5ab45571f711b910cb23065a568c6a
amelia <44528100+pozm@users.noreply.github.com>
2024-10-19 16:17:43 +0100

    im a meower

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,14 @@
 use backend::{db::migrate::do_migration, register_tracing};
+use dotenvy::dotenv;
 use tokio::net::TcpListener;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
     register_tracing();
+    if dotenv().is_err() {
+        info!("No .env file found, using defaults");
+    }
     do_migration().await;
     let app = axum::Router::new().route("/", axum::routing::get(index));
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
